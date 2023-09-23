da7a7fb9c23c40f7aad42cdd2d47231dae8f9816
Ama Ranasinghe <amaprabhashini926@gmail.com>
2023-09-24 00:09:50 +0530

    Initial Commit

---
+++ expenseTrackTest/expenseTrackTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenseTrackTestApp.swift
+//  expenseTrackTest
+//
+//  Created by Ama Ranasi on 2023-09-24.
+//
+
+import SwiftUI
+
+@main
+struct expenseTrackTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
